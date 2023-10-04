3978bbaa9c8f8c8236608d4bfa3dc72d213a9dc5
UnMugViolet <jaguinpaul@gmail.com>
2023-10-04 11:13:34 +0200

    🚨 Loading page work

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import ContentCenter from './layouts/ContentCenter.vue'
+
+import Loading from './components/Loading.vue'
 </script>
 
 <template>
+    <ContentCenter class="bg-black">
+        <template #center>
+            <div class="md:w-1/3 w-48 ml-5">
+                <img src="src/assets/img/logo-portfolio-dark.svg" alt="logo démarrage">
+            </div>
+        </template>
 
-    <h1 class=""> LALALALALAL</h1>
+        <template #bottom-center>
+            <Loading />
+        </template>
+
+        <template #bottom>
+            <div class="app-container absolute bottom-0 my-16">
+                    <div>
+                        <div class="flex justify-between gap-1.5">
+                            <div>
+                                <h1 class="text-white md:text-xl text-sm">
+                                    Bienvenu sur mon ordinateur.
+                                </h1>
+                            </div>
+                            <div>
+                                <h2 class="text-white md:text-2xl text-md logo-font">
+                                    UnMugViolet
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+        </template>
+
+    </ContentCenter>
 </template>
