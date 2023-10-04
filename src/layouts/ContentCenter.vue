055fcf737274919c3ff751e4e8a6089c84904f0e
UnMugViolet <jaguinpaul@gmail.com>
2023-10-04 11:10:55 +0200

    Work on login page

---
+++ src/layouts/ContentCenter.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="h-screen w-screen">
+        <div class="flex items-center justify-center h-full w-full">
+            <div class="app-container">
+                <div class="flex flex-col items-center justify-center w-full h-full">
+                    <slot name="top"></slot> 
+                    <slot name="center"></slot> 
+
+                    <slot name="bottom-center"></slot> 
+                </div>
+                <slot name="bottom"></slot> 
+            </div>
+        </div>
+    </section>
+</template>
