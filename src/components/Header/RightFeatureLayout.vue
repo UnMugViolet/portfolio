331b6d7f9afeb6be0e6c7b65c17c28e80ba1e507
UnMugViolet <jaguinpaul@gmail.com>
2023-10-11 11:19:19 +0200

    ♻️ Refacto composition api

---
+++ src/components/Header/RightFeatureLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="flex w-full h-10 items-center gap-1.5 px-0.5 component-style">
+        <div class="w-9 h-9">
+            <slot name="img" />
+        </div>
+        <div class="text-left">
+            <h3 class="header-font text-xs">
+                <slot name="title" />
+            </h3>
+            <p class="subtitle">
+                <slot name="subtitle" />
+            </p>
+        </div>
+    </button>
+</template>
+
+<style scoped>
+    .component-style:hover {
+        background: #2F71CD;
+        color: white;
+    }
+    
+    .component-style:hover .subtitle {
+        color: white;
+    }
+
+    .subtitle {
+        font-size: 11px;
+        font-weight: 400;
+        color: #868686;
+    }
+</style>
