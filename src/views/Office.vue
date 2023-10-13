b5d9f89f66f604bb055152de6d033c6a2433770e
UnMugViolet <jaguinpaul@gmail.com>
2023-10-13 19:51:09 +0200

    💩 Started working on the Header modal

--- src/views/Office.vue
+++ src/views/Office.vue
@@ -1,23 +1,37 @@
 <template>
     <section class="h-screen w-screen overflow-hidden bg-office-pic relative">
-        <Header />
-        <div>
-
-        </div>
-        <Footer />
+      <Header v-if="showHeader" />
+      <div></div>
+      <Footer @toggle-header="toggleHeader" />
     </section>
-
-</template>
-
-<script>
-import Footer from '../components/Footer/Footer.vue';
-import Header from '../components/Header/Header.vue';
-
-export default {
-    name: 'Office',
-    components: {
-        Footer,
-        Header,
+  </template>
+  
+  <script setup>
+  import Footer from '../components/Footer/Footer.vue';
+  import Header from '../components/Header/Header.vue';
+  import { ref, onMounted, onBeforeUnmount } from 'vue';
+  
+  const showHeader = ref(false);
+  
+  const toggleHeader = () => {
+    showHeader.value = !showHeader.value;
+  };
+  const closeHeader = () => {
+    showHeader.value = false;
+  };
+  
+  const handleDocumentClick = (event) => {
+    if (!document.querySelector('.header-component').contains(event.target)) {
+      closeHeader();
     }
-};
-</script>
+  };
+  
+  onMounted(() => {
+    document.addEventListener('click', handleDocumentClick);
+  });
+  
+  onBeforeUnmount(() => {
+    document.removeEventListener('click', handleDocumentClick);
+  });
+  </script>
+  
