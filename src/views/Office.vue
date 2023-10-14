73d4ccd10021045ec10f76785549d67ed633ac81
UnMugViolet <jaguinpaul@gmail.com>
2023-10-14 11:46:10 +0200

    ♻️Header toggle refacto work now

--- src/views/Office.vue
+++ src/views/Office.vue
@@ -1,37 +1,42 @@
 <template>
-    <section class="h-screen w-screen overflow-hidden bg-office-pic relative">
-      <Header v-if="showHeader" />
-      <div></div>
-      <Footer @toggle-header="toggleHeader" />
-    </section>
-  </template>
-  
-  <script setup>
+  <section
+    class="h-screen w-screen overflow-hidden bg-office-pic relative"
+    @click="handleOutsideClick"
+  >
+    <Header v-if="showHeader" @click="toggleHeader" />
+    <div></div>
+    <Footer @toggle-header="toggleHeader" />
+  </section>
+</template>
+
+<script setup>
   import Footer from '../components/Footer/Footer.vue';
   import Header from '../components/Header/Header.vue';
-  import { ref, onMounted, onBeforeUnmount } from 'vue';
-  
+  import { ref } from 'vue';
+
   const showHeader = ref(false);
-  
+
   const toggleHeader = () => {
     showHeader.value = !showHeader.value;
   };
-  const closeHeader = () => {
-    showHeader.value = false;
-  };
-  
-  const handleDocumentClick = (event) => {
-    if (!document.querySelector('.header-component').contains(event.target)) {
-      closeHeader();
+
+  const handleOutsideClick = (event) => {
+    if (showHeader.value) {
+      const headerElement = document.querySelector('.header-component');
+      const startButtonElement = document.querySelector('.start-button');
+
+      // Check if the clicked element is the StartButton
+      if (
+        startButtonElement &&
+        startButtonElement.contains(event.target)
+      ) {
+        return; 
+      }
+
+      // Close the header if the clicked element is outside the header
+      if (headerElement && !headerElement.contains(event.target)) {
+        toggleHeader();
+      }
     }
   };
-  
-  onMounted(() => {
-    document.addEventListener('click', handleDocumentClick);
-  });
-  
-  onBeforeUnmount(() => {
-    document.removeEventListener('click', handleDocumentClick);
-  });
-  </script>
-  
+</script>
