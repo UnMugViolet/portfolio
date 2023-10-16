238a98f77bf3333ced63f0293524b1e58badef8b
UnMugViolet <jaguinpaul@gmail.com>
2023-10-16 20:19:53 +0200

    Fix merge issue

--- src/views/Office.vue
+++ src/views/Office.vue
@@ -1,44 +1,45 @@
 <template>
-  <section
-    class="h-screen w-screen overflow-hidden bg-office-pic relative"
-    @click="handleOutsideClick"
-  >
-    <Header v-if="showHeader" @toggle-header="toggleHeader"/>
-    <div></div>
-    <Footer @toggle-header="toggleHeader" />
-  </section>
-</template>
-
-<script setup>
-  import Footer from '../components/Footer/Footer.vue';
-  import Header from '../components/Header/Header.vue';
-  import { ref } from 'vue';
-
-  const showHeader = ref(true);
-
-  const toggleHeader = () => {
-    showHeader.value = !showHeader.value;
-  };
-
-  const handleOutsideClick = (event) => {
-    if (showHeader.value) {
-      const headerElement = document.querySelector('.header-component');
-      const startButtonElement = document.querySelector('.start-button');
-      const fullScreenElement = document.querySelector('.full-screen');
-
-      // Check if the clicked element is the StartButton or FullScreen
-      if (
-        (startButtonElement && startButtonElement.contains(event.target)) ||
-        (fullScreenElement && fullScreenElement.contains(event.target))
-      ) {
-      return;
-    }
-
-      // Close the header if the clicked element is outside the header
-      if (headerElement && !headerElement.contains(event.target)) {
-        toggleHeader();
+    <section
+      class="h-screen w-screen overflow-hidden bg-office-pic relative"
+      @click="handleOutsideClick"
+    >
+      <Header v-if="showHeader" @toggle-header="toggleHeader"/>
+      <div></div>
+      <Footer @toggle-header="toggleHeader" />
+    </section>
+  </template>
+  
+  <script setup>
+    import Footer from '../components/Footer/Footer.vue';
+    import Header from '../components/Header/Header.vue';
+    import { ref } from 'vue';
+  
+    const showHeader = ref(true);
+  
+    const toggleHeader = () => {
+      showHeader.value = !showHeader.value;
+    };
+  
+    const handleOutsideClick = (event) => {
+      if (showHeader.value) {
+        const headerElement = document.querySelector('.header-component');
+        const startButtonElement = document.querySelector('.start-button');
+        const fullScreenElement = document.querySelector('.full-screen');
+  
+        // Check if the clicked element is the StartButton or FullScreen
+        if (
+          (startButtonElement && startButtonElement.contains(event.target)) ||
+          (fullScreenElement && fullScreenElement.contains(event.target))
+        ) {
+        return;
       }
-    }
-  };
-
-</script>
+  
+        // Close the header if the clicked element is outside the header
+        if (headerElement && !headerElement.contains(event.target)) {
+          toggleHeader();
+        }
+      }
+    };
+  
+  </script>
+  
