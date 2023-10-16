28b7edc9a0431f19696632944791a4fbdcde7f80
UnMugViolet <jaguinpaul@gmail.com>
2023-10-16 20:02:39 +0200

    Revert "Merge pull request #5 from UnMugViolet/feature-header"
    This reverts commit 7973bac157cb70c3c898b4cfb530119b38c7df9b, reversing
    changes made to a95a4ed0b4a53b332af4d13fe502280cc71d39d8.

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,11 +1,17 @@
+<script setup>    
+    import StartButton from '../Buttons/StartButton.vue';
+    import FooterRight from './FooterRight.vue';
+</script>
+
+
 <template>
     <footer>
-      <div class="absolute bottom-0 footer-gradient shadow-footer w-full z-20">
-        <div class="h-8 flex items-center justify-between">
-          <StartButton @click="toggleHeader" />
-          <FooterRight />
+        <div class="absolute bottom-0 footer-gradient shadow-footer w-full z-20">
+            <div class="h-8 flex items-center justify-between"> 
+                <StartButton />
+                <FooterRight />
+            </div>
         </div>
-      </div>
     </footer>
   </template>  
 
