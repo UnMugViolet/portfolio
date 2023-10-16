0b9435d27504c8408c998852903e82eb273669db
UnMugViolet <jaguinpaul@gmail.com>
2023-10-16 18:44:11 +0200

    🎉 Refacto the header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -92,7 +92,20 @@
                         </div>
                     </div>
                 </section>
-                <BottomHeader />
+                <div class="bg absolute bottom-0 h-12 w-full ">
+                    <div class="header-bot-background h-full flex justify-end items-center">
+                        <div class="flex h-5/6 gap-3 mr-2">
+                            <button class="flex text-white text-xs h-full items-center buttons-header-bottom px-1 full-screen" @click="turnOffHeader">
+                                <img src="src/assets/img/icons/key-log-icon.png" alt="Se déconnecter" class="w-fit h-4/6 mr-0.5"/>
+                                <p class="small-p">Se Déconnecter</p>
+                            </button>
+                            <button class="flex text-white text-xs h-full items-center buttons-header-bottom px-1 full-screen" @click="turnOffHeader" >
+                                <img src="src/assets/img/icons/shutdown-icon.png" alt="Éteindre l'ordinateur" class="w-fit h-4/6 mr-0.5"/>
+                                <p class="small-p">Éteindre L'ordinateur</p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
@@ -102,7 +115,6 @@
     import ProfilePicture from '../ProfilePicture.vue';
     import LeftFeatureLayout from './LeftFeatureLayout.vue';
     import RightFeatureLayout from './RightFeatureLayout.vue';
-    import BottomHeader from './BottomHeader.vue';
 
     const emit = defineEmits();
 
