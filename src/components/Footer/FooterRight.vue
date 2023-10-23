7d62a8e60d30a4f7be6940b793b2ffec20c03370
UnMugViolet <jaguinpaul@gmail.com>
2023-10-23 16:08:38 +0200

    🗑️ Clean up the repo

--- src/components/Footer/FooterRight.vue
+++ src/components/Footer/FooterRight.vue
@@ -1,6 +1,6 @@
 <script setup>
   import { ref } from 'vue';
-  import CurrentTime from '../CurrentTime.vue';
+  import CurrentTime from './CurrentTime.vue';
 
   // Initialize refs
   const isFullScreen = ref(false);
