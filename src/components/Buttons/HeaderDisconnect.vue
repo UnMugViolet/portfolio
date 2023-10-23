da849a0c743a92ca3b8e0713290eef794f31d643
UnMugViolet <jaguinpaul@gmail.com>
2023-10-23 12:12:00 +0200

    ♻️ Refacto buttons header

---
+++ src/components/Buttons/HeaderDisconnect.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="flex text-white text-xs h-full items-center buttons-header-bottom px-1 full-screen">
+        <img src="src/assets/img/icons/key-log-icon.png" alt="Se déconnecter" class="w-fit h-4/6 mr-0.5"/>
+        <p class="small-p">Se Déconnecter</p>
+    </button>
+</template>
---
+++ src/components/Buttons/HeaderShutdown.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="flex text-white text-xs h-full items-center buttons-header-bottom px-1 full-screen">
+        <img src="src/assets/img/icons/shutdown-icon.png" alt="Éteindre l'ordinateur" class="w-fit h-4/6 mr-0.5"/>
+        <p class="small-p">Éteindre L'ordinateur</p>
+    </button>
+</template>
