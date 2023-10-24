515bb42a9b0ca2318a9d79ab6601c7d56de7cc55
UnMugViolet <jaguinpaul@gmail.com>
2023-10-24 20:21:25 +0200

    ✏️ Fix typo

--- src/layouts/Window.vue
+++ src/layouts/Window.vue
@@ -2,7 +2,7 @@
     <section class="absolute radius-window overflow-hidden window-size inset-36 bg-window-blue z-30 ">
         <div class="absolute top-0 left-0 linear-header-window h-8 w-full z-40"></div>
         <div class="absolute w-full h-full flex overflow-hidden p-0.5">
-            <div class="relative top-0 w-full h-7 mt-6 linear-backgroud-window">
+            <div class="relative top-0 w-full h-7 mt-6 linear-background-functions">
     
             </div>
         </div>
@@ -32,7 +32,7 @@
        background: linear-gradient(rgb(0, 88, 238) 0%, rgb(53, 147, 255) 4%, rgb(40, 142, 255) 6%, rgb(18, 125, 255) 8%, rgb(3, 111, 252) 10%, rgb(2, 98, 238) 14%, rgb(0, 87, 229) 20%, rgb(0, 84, 227) 24%, rgb(0, 85, 235) 56%, rgb(0, 91, 245) 66%, rgb(2, 106, 254) 76%, rgb(0, 98, 239) 86%, rgb(0, 82, 214) 92%, rgb(0, 64, 171) 94%, rgb(0, 48, 146) 100%);
     }
 
-    .linear-backgroud-window{
+    .linear-background-functions{
         background: linear-gradient(to right, rgb(237, 237, 229) 0%, rgb(237, 232, 205) 100%);
     }
 </style>
