24c5319e72e7b5fcbe25a9f9360499333ca6ccc9
UnMugViolet <jaguinpaul@gmail.com>
2023-10-26 16:57:56 +0200

    Switch to composition Api on button

--- src/components/Buttons/WindowMaximize.vue
+++ src/components/Buttons/WindowMaximize.vue
@@ -7,22 +7,39 @@
     </button>
   </template>
   
-
-<style scoped>
-    .maximize-button{
-        box-shadow: rgb(70, 70, 255) 0px -1px 2px 1px inset;
-        background-image: radial-gradient(circle at 90% 90%, rgb(0, 84, 233) 0%, rgb(34, 99, 213) 55%, rgb(68, 121, 228) 70%, rgb(163, 187, 236) 90%, white 100%);
-    }
-
-    .maximize-button::before{
-        content: "";
-        box-shadow: white 0px 3px inset, white 0px 0px 0px 1px inset;
-        height: 13px;
-        width: 13px;
-    }
-
-
-  .maximize-button.clicked::after{
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const isClicked = ref(false);
+  
+      const toggleButton = () => {
+        isClicked.value = !isClicked.value;
+      };
+  
+      return {
+        isClicked,
+        toggleButton,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .maximize-button {
+    box-shadow: rgb(70, 70, 255) 0px -1px 2px 1px inset;
+    background-image: radial-gradient(circle at 90% 90%, rgb(0, 84, 233) 0%, rgb(34, 99, 213) 55%, rgb(68, 121, 228) 70%, rgb(163, 187, 236) 90%, white 100%);
+  }
+  
+  .maximize-button::before {
+    content: "";
+    box-shadow: white 0px 3px inset, white 0px 0px 0px 1px inset;
+    height: 13px;
+    width: 13px;
+  }
+  
+  .maximize-button.clicked::after {
     content: "";
     position: absolute;
     display: block;
@@ -31,10 +48,10 @@
     box-shadow: white 0px 2px inset, white 0px 0px 0px 1px inset, rgb(19, 109, 255) 1px -1px;
     height: 8px;
     width: 8px;
-    background-color: rgb(19, 109, 255); 
+    background-color: rgb(19, 109, 255);
   }
-
-  .maximize-button.clicked::before{
+  
+  .maximize-button.clicked::before {
     content: "";
     position: absolute;
     display: block;
@@ -43,22 +60,6 @@
     box-shadow: white 0px 2px inset, white 0px 0px 0px 1px inset;
     height: 8px;
     width: 8px;
-}
-
-</style>
-
-<script>
-export default {
-  data() {
-    return {
-      isClicked: false,
-    };
-  },
-  methods: {
-    toggleButton() {
-      this.isClicked = !this.isClicked;
-    },
-  },
-};
-</script>
-
+  }
+  </style>
+  
