faad437b8947ec5192b3578a4c88c492e23230a4
UnMugViolet <jaguinpaul@gmail.com>
2024-01-03 12:31:20 +0100

    ♻️ Update the resize window and refactor code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         'window-white': 'linear-gradient(to right, rgb(237, 237, 229) 0%, rgb(237, 232, 205) 100%)',
       }),
       colors: {
-        dropdown: '#1660E8',
+        'dropdown': '#1660E8',
+        'window-blue': '#0831D9'
       },
       padding: {
         '0.75': '0.19rem',
