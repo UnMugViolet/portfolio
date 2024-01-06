0bf1025c18eb1e2cf4f6db359e6af1fe69ce1b0a
UnMugViolet <jaguinpaul@gmail.com>
2024-01-06 13:28:13 +0100

    🎨 Improve css with multiple pellets

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,13 +2,51 @@
   <footer>
     <div class="absolute bottom-0 footer-gradient shadow-footer w-full z-50">
       <div class="h-8 flex items-center justify-between">
-        <div class="flex items-center h-full ">
+        <div class="flex items-center h-full">
           <StartButton @click="toggleHeader" />
-          <div class="flex items-center h-full gap-1 px-2 py-1 mt-px w-44">
-            <div class="flex items-center px-2 w-full h-full bg-pellet-blue box-shadow-pellet-footer hover:brightness-110 rounded-sm">
-              <div class="flex gap-1 mt-px">
-                <img src="/src/assets/img/icons/projects-icon-xs.png" alt="office-icon" class="w-4 h-4"/>
-                <p class="small-p text-white">My Projects</p>
+          <div class="flex w-full h-full ml-2 gap-0.5">
+            <div class="flex items-center h-full gap-1 py-1 mt-px w-44">
+              <div class="flex items-center px-2 w-full h-full bg-pellet-blue box-shadow-pellet-footer hover:brightness-110 rounded-sm">
+                <div class="flex gap-1 mt-px">
+                  <img src="/src/assets/img/icons/projects-icon-xs.png" alt="office-icon" class="w-4 h-4"/>
+                  <p class="small-p text-white">My Projects</p>
+                </div>
+              </div>
+            </div>
+  
+            <div class="flex items-center h-full gap-1 py-1 mt-px w-44">
+              <div class="flex items-center px-2 w-full h-full bg-pellet-blue box-shadow-pellet-footer hover:brightness-110 rounded-sm">
+                <div class="flex gap-1 mt-px">
+                  <img src="/src/assets/img/icons/projects-icon-xs.png" alt="office-icon" class="w-4 h-4"/>
+                  <p class="small-p text-white">My Projects</p>
+                </div>
+              </div>
+            </div>
+  
+            <div class="flex items-center h-full gap-1 py-1 mt-px w-44">
+              <div class="flex items-center px-2 w-full h-full bg-pellet-blue box-shadow-pellet-footer hover:brightness-110 rounded-sm">
+                <div class="flex gap-1 mt-px">
+                  <img src="/src/assets/img/icons/projects-icon-xs.png" alt="office-icon" class="w-4 h-4"/>
+                  <p class="small-p text-white">My Projects</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="flex items-center h-full gap-1 py-1 mt-px w-44">
+              <div class="flex items-center px-2 w-full h-full bg-pellet-blue box-shadow-pellet-footer hover:brightness-110 rounded-sm">
+                <div class="flex gap-1 mt-px">
+                  <img src="/src/assets/img/icons/projects-icon-xs.png" alt="office-icon" class="w-4 h-4"/>
+                  <p class="small-p text-white">My Projects</p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="flex items-center h-full gap-1 py-1 mt-px w-44">
+              <div class="flex items-center px-2 w-full h-full bg-pellet-blue box-shadow-pellet-footer hover:brightness-110 rounded-sm">
+                <div class="flex gap-1 mt-px">
+                  <img src="/src/assets/img/icons/projects-icon-xs.png" alt="office-icon" class="w-4 h-4"/>
+                  <p class="small-p text-white">My Projects</p>
+                </div>
               </div>
             </div>
           </div>
