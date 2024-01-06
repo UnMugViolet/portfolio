67af6fc19f7b299519bc0ed69ff963430b64cdbe
UnMugViolet <jaguinpaul@gmail.com>
2024-01-06 12:56:20 +0100

    ✨ Added the components to each windows

--- src/components/Window/MyCV.vue
+++ src/components/Window/MyCV.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <div class="relative right-0 h-content-window flex">
+        <div class="bg-window-right-component w-194px h-full flex-shrink-0">
+        
+        </div>
+        <div class="w-full h-full bg-white overflow-auto">
+            
+        </div>
+    </div>  
 </template>
 
 <script setup>
