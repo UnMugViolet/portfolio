c1b0c588cf24d3519e0666b786468bf18afef51e
UnMugViolet <jaguinpaul@gmail.com>
2024-01-11 18:34:50 +0100

    ➖ RM useless dependencie

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import '../css/main.css'
 import '../src/index.css'
-import vClickOutside from 'v-click-outside'
 
 import { createApp } from 'vue' 
 import App from './App.vue'
@@ -9,6 +8,5 @@ import router from './router'
 const app = createApp(App);
 
 app.use(router);
-app.use(vClickOutside)
 
 app.mount('#app');
