e9084877654b72598e98be248a14124016d7c6b2
UnMugViolet <jaguinpaul@gmail.com>
2024-03-30 20:04:47 +0100

    Added vue devtools

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevTools(),
     vueJsx(),
     nightwatchPlugin({
       renderPage: './nightwatch/index.html'
