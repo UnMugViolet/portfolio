2eb6c6a7a48ca4aa20505246bcd1f15ffeacc7fc
UnMugViolet <jaguinpaul@gmail.com>
2024-03-30 20:05:15 +0100

    🚧 Started to work on card menu window

--- src/components/Window/MyProjects.vue
+++ src/components/Window/MyProjects.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="relative right-0 h-content-window flex">
-        <div class="bg-window-right-component w-194px h-full flex-shrink-0">
-        
+        <div class="bg-window-right-component w-194px h-full flex-shrink-0 flex-col p-2.5 gap-5">
+            <div class="text-twilight-blue">
+                <div class="bg-window-menu-card-header rounded-t-sm px-2 text-sm font-bold">
+                    Blabla
+                </div>
+                <div class="text-xs p-1 bg-window-menu-card px-2"> 
+                    <div class="flex flex-row gap-2">
+                        <img src="assets/icons/plus.svg" alt="plus" class="w-3 h-3">
+                        <p class="font-light text-xxs">Blabla</p>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full h-full bg-white overflow-auto">
             
         </div>
     </div>  
 </template>
+
