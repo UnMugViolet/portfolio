409b816f1080a4b75fdbe314128c833ef0dcbb6d
UnMugViolet <jaguinpaul@gmail.com>
2024-04-10 21:59:36 +0200

    📱 Work on responsive footer, still shitty on mobile

--- src/components/Footer/FooterRight.vue
+++ src/components/Footer/FooterRight.vue
@@ -1,6 +1,36 @@
+<script setup>
+  import { ref } from 'vue';
+  import CurrentTime from './CurrentTime.vue';
+
+  // Initialize refs
+  const isFullScreen = ref(false);
+  const originalTitle = ref('Mode plein écran');
+
+  const enterFullScreen = () => {
+    if (isFullScreen.value) {
+      // Exit full-screen mode
+      document.exitFullscreen();
+      originalTitle.value = 'Mode plein écran';
+      isFullScreen.value = false;
+    } else {
+      // Enter full-screen mode
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+      } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+      }
+      originalTitle.value = 'Quitter le mode plein écran';
+      isFullScreen.value = true;
+    }
+  };
+</script>
 
 <template>
-  <div class="text-white h-full flex items-center px-3 gap-1 bg-footer-right-component footer-left-shadow select-none">
+  <div class="text-white h-full flex items-center px-2 sm:px-3 gap-1 bg-footer-right-component footer-left-shadow select-none">
     <img
       class="w-4 h-4 cursor-pointer"
       src="@/assets/img/icons/full-screen-icon-lg.png"
@@ -12,34 +42,3 @@
     <CurrentTime />
   </div>
 </template>
-
-      <script setup>
-        import { ref } from 'vue';
-        import CurrentTime from './CurrentTime.vue';
-      
-        // Initialize refs
-        const isFullScreen = ref(false);
-        const originalTitle = ref('Mode plein écran');
-      
-        const enterFullScreen = () => {
-          if (isFullScreen.value) {
-            // Exit full-screen mode
-            document.exitFullscreen();
-            originalTitle.value = 'Mode plein écran';
-            isFullScreen.value = false;
-          } else {
-            // Enter full-screen mode
-            if (document.documentElement.requestFullscreen) {
-              document.documentElement.requestFullscreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-              document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullscreen) {
-              document.documentElement.webkitRequestFullscreen();
-            } else if (document.documentElement.msRequestFullscreen) {
-              document.documentElement.msRequestFullscreen();
-            }
-            originalTitle.value = 'Quitter le mode plein écran';
-            isFullScreen.value = true;
-          }
-        };
-      </script>
