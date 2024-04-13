5bdeed4d1e6f749f0458069329c5dd0e9365891e
UnMugViolet <jaguinpaul@gmail.com>
2024-04-14 00:36:06 +0200

    🛂 Create notification modal

--- src/components/Modals/NotificationModal.vue
+++ src/components/Modals/NotificationModal.vue
@@ -1,7 +1,7 @@
-<script setup> 
-
-</script>
-
 <template>
-
+  <Transition>
+    <div class="absolute right-20 bottom-11 border border-black bg-[#FFFFE1] rounded-lg p-2 text-xs whitespace-nowrap">
+      <p>Test test</p>
+    </div>
+  </Transition>
 </template>
