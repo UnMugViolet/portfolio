9bc71abc088bc5c1fae4cc55b2f260bcbf8cb3ca
UnMugViolet <jaguinpaul@gmail.com>
2024-04-23 22:38:49 +0200

    🎉 Start to work on activeness

--- src/components/Window/MyProjects.vue
+++ src/components/Window/MyProjects.vue
@@ -8,6 +8,10 @@ const props = defineProps({
 
 const categories = projectData.categories;
 
+const activateProject = (project) => {
+    project.isActive = !project.isActive;
+};
+
 </script>
 
 <template>
@@ -20,9 +24,16 @@ const categories = projectData.categories;
                 <div class="absolute left-[-12px] top-5 w-80 h-px bg-gradient-to-r from-blue-300 to-white"></div>
 
                 <!-- Content for the projects -->
-                <div class="flex flex-wrap gap-2 pt-2 md:pt-3 pb-5 w-full ">
-                    <div v-for="project in category.projects" :key="project.name" class="flex items-center px-4 gap-2.5">
-                        <img :src="'src/assets/img/icons/' + project.icon" alt="project" class="w-10 h-10"/>
+                <div class="flex flex-wrap gap-2 pt-2 md:pt-3 pb-5 w-full cursor-pointer">
+                    <div 
+                        v-for="project in category.projects" :key="project.name" 
+                        @click="activateProject(project)"
+                        class="flex items-center px-4 gap-2.5"
+                        :class="{ active: project.isActive }"
+                        :style="{
+                            backgroundColor: project.isActive ? 'rgb(11, 97, 255)' : 'transparent',
+                        }">
+                        <img :src="'src/assets/img/icons/' + project.icon" alt="project" class="w-10 h-10" :style="{opacity: project.isActive ? 0.5 : 1}"/>
                         <p class="text-xs font-tahoma font-medium">
                             {{ project.name }}
                         </p>
@@ -32,3 +43,9 @@ const categories = projectData.categories;
         </div>
     </div>  
 </template>
+
+<style scoped>
+  .active {
+    filter: drop-shadow(blue 0px 0px);
+  }
+</style>
