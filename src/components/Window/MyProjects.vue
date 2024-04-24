4025e30b1679fd77c3cde4322f3b333060de97fc
UnMugViolet <jaguinpaul@gmail.com>
2024-04-24 16:56:25 +0200

    Info showing  up in the window

--- src/components/Window/MyProjects.vue
+++ src/components/Window/MyProjects.vue
@@ -19,7 +19,7 @@ const activateProject = (project) => {
   if(project.isActive) {
     return;
   }
-  
+
   project.isActive = !project.isActive;
 
   // Set all other projects to false
@@ -30,16 +30,27 @@ const activateProject = (project) => {
       }
     });
   });
+  selectedProject = project;
+};
+
+let selectedProject = null;
+
+const toggleProject = (project) => {
+  selectedProject = project;
 };
 
 </script>
 
 <template>
-  <div class="relative right-0 h-content-window flex"
-    >
+  <div class="relative right-0 h-content-window flex">
     <WindowSideMenu :subMenuItems="props.subMenuItems" />
+    <!-- Content of project -->
+    <div v-if="selectedProject" class="w-full h-full bg-white">
+      <h2>{{ selectedProject.title }}</h2>
+      {{ selectedProject.description }}
+    </div>
     <!-- Content window Foreach categories and projects -->
-    <div class="flex flex-col w-full h-full bg-white overflow-auto pt-0.5">
+    <div v-else class="flex flex-col w-full h-full bg-white overflow-auto pt-0.5">
       <div v-for="category in categories" :key="category.name" class="relative group mb-3">
         <h1 class="text-xs font-semibold px-3">{{ category.name }}</h1>
         <div class="absolute left-[-12px] top-5 w-80 h-px bg-gradient-to-r from-blue-300 to-white"></div>
@@ -50,8 +61,10 @@ const activateProject = (project) => {
             v-for="project in category.projects" 
             :key="project.name" 
             @click.stop="activateProject(project)"
+            @dblclick="toggleProject(project)"
             class="flex items-center px-4 gap-2.5" 
             :class="{ active: project.isActive }">
+
             <img :src="'src/assets/img/icons/' + project.icon" alt="project" class="w-10 h-10"
               :style="{ opacity: project.isActive ? 0.5 : 1 }" />
             <p class="text-xs font-tahoma font-medium" :style="{
