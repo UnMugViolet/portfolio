0b2a7c62d56c60231a0adaf332b5c8667c5f2485
UnMugViolet <jaguinpaul@gmail.com>
2024-04-24 12:22:43 +0200

    ✨ Principle of activeness is ready

--- src/components/Window/MyProjects.vue
+++ src/components/Window/MyProjects.vue
@@ -1,51 +1,75 @@
 <script setup>
+import { reactive } from 'vue';
 import projectData from '@/data/projects-data.json';
 import WindowSideMenu from '@/components/Window/WindowSideMenu.vue'
 
 const props = defineProps({
-    subMenuItems: Array,
+  subMenuItems: Array,
 });
 
-const categories = projectData.categories;
+const categories = reactive(projectData.categories.map(category => ({
+  // Copy the category object
+  ...category,
+  // Add a new property to each project
+  projects: category.projects.map(project => ({ ...project, isActive: false }))
+})));
 
 const activateProject = (project) => {
-    project.isActive = !project.isActive;
+
+  if(project.isActive) {
+    return;
+  }
+  
+  project.isActive = !project.isActive;
+
+  // Set all other projects to false
+  categories.forEach((category) => {
+    category.projects.forEach((p) => {
+      if (p.name !== project.name) {
+        p.isActive = false;
+      }
+    });
+  });
 };
 
 </script>
 
 <template>
-    <div class="relative right-0 h-content-window flex">
-        <WindowSideMenu :subMenuItems="props.subMenuItems"/>
-        <!-- Content window Foreach categories and projects -->
-        <div class="flex flex-col w-full h-full bg-white overflow-auto pt-0.5">
-            <div v-for="category in categories" :key="category.name" class="relative group mb-3">
-                <h1 class="text-xs font-semibold px-3">{{ category.name }}</h1>
-                <div class="absolute left-[-12px] top-5 w-80 h-px bg-gradient-to-r from-blue-300 to-white"></div>
+  <div class="relative right-0 h-content-window flex"
+    >
+    <WindowSideMenu :subMenuItems="props.subMenuItems" />
+    <!-- Content window Foreach categories and projects -->
+    <div class="flex flex-col w-full h-full bg-white overflow-auto pt-0.5">
+      <div v-for="category in categories" :key="category.name" class="relative group mb-3">
+        <h1 class="text-xs font-semibold px-3">{{ category.name }}</h1>
+        <div class="absolute left-[-12px] top-5 w-80 h-px bg-gradient-to-r from-blue-300 to-white"></div>
 
-                <!-- Content for the projects -->
-                <div class="flex flex-wrap gap-2 pt-2 md:pt-3 pb-5 w-full cursor-pointer">
-                    <div 
-                        v-for="project in category.projects" :key="project.name" 
-                        @click="activateProject(project)"
-                        class="flex items-center px-4 gap-2.5"
-                        :class="{ active: project.isActive }"
-                        :style="{
-                            backgroundColor: project.isActive ? 'rgb(11, 97, 255)' : 'transparent',
-                        }">
-                        <img :src="'src/assets/img/icons/' + project.icon" alt="project" class="w-10 h-10" :style="{opacity: project.isActive ? 0.5 : 1}"/>
-                        <p class="text-xs font-tahoma font-medium">
-                            {{ project.name }}
-                        </p>
-                    </div>
-                </div>
-            </div>
+        <!-- Content for the projects -->
+        <div class="flex flex-wrap gap-2 pt-2 md:pt-3 pb-5 w-full cursor-pointer">
+          <div 
+            v-for="project in category.projects" 
+            :key="project.name" 
+            @click.stop="activateProject(project)"
+            class="flex items-center px-4 gap-2.5" 
+            :class="{ active: project.isActive }">
+            <img :src="'src/assets/img/icons/' + project.icon" alt="project" class="w-10 h-10"
+              :style="{ opacity: project.isActive ? 0.5 : 1 }" />
+            <p class="text-xs font-tahoma font-medium" :style="{
+              backgroundColor: project.isActive ? 'rgb(11, 97, 255)' : 'transparent',              
+              color: project.isActive ? 'white' : 'black',
+
+            }">
+              {{ project.name }}
+            </p>
+          </div>
         </div>
-    </div>  
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-  .active {
-    filter: drop-shadow(blue 0px 0px);
-  }
+.active {
+  filter: drop-shadow(blue 0px 0px);
+}
 </style>
