461cc89b7e1ef2bb540fccdb5e87bb54516c09e6
UnMugViolet <jaguinpaul@gmail.com>
2024-05-05 22:24:47 +0200

    Data from CV is comming from json

---
+++ src/components/Windows/MyCV/EducationItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mt-2 mb-5">
+    <h4 class="font-trebuchet-pixel uppercase text-xs">{{ education.degree }}</h4>
+    <p class="font-trebuchet-pixel font-medium text-xs">{{ education.institution }} | {{ education.year }}</p>
+    <p class="font-trebuchet-pixel font-medium text-xs">{{ education.description }}</p>
+  </div>
+</template>
+
+
+<script setup>
+const props = defineProps({
+  education: Object,
+});
+</script>
---
+++ src/components/Windows/MyCV/WorkExperienceItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4 class="font-trebuchet-pixel uppercase text-xs mt-5">{{ workExperience.jobTitle }}</h4>
+    <p class="font-trebuchet-pixel font-medium text-xs"> {{ workExperience.company }} | {{ workExperience.year }}</p>
+    <ul class="font-trebuchet-pixel list-disc pl-5 text-xs">
+      <li v-for="(responsibility, index) in workExperience.responsibilities" :key="index">
+        {{ responsibility }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  workExperience: Object,
+});
+</script>
