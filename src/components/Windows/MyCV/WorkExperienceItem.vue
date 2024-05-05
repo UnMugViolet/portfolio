78ed4017dad694006e3a036d14b42a2c6a7bf3f2
UnMugViolet <jaguinpaul@gmail.com>
2024-05-05 22:25:22 +0200

    Rearrange style

--- src/components/Windows/MyCV/WorkExperienceItem.vue
+++ src/components/Windows/MyCV/WorkExperienceItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h4 class="font-trebuchet-pixel uppercase text-xs mt-5">{{ workExperience.jobTitle }}</h4>
+  <div class="mt-2 mb-5">
+    <h4 class="font-trebuchet-pixel uppercase text-xs">{{ workExperience.jobTitle }}</h4>
     <p class="font-trebuchet-pixel font-medium text-xs"> {{ workExperience.company }} | {{ workExperience.year }}</p>
     <ul class="font-trebuchet-pixel list-disc pl-5 text-xs">
       <li v-for="(responsibility, index) in workExperience.responsibilities" :key="index">
