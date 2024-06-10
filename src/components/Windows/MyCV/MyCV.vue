df7a287bfac3852613a5d27936497228961b5e15
UnMugViolet <jaguinpaul@gmail.com>
2024-06-10 17:47:13 +0200

    ✨ Load projects descriptions dynamically

--- src/components/Windows/MyCV/MyCV.vue
+++ src/components/Windows/MyCV/MyCV.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import ProfileHeader from './ProfileHeader.vue';
+import EducationItem from './EducationItem.vue';
+import cvData from '@/data/cv-data.json';
+import WorkExperienceItem from './WorkExperienceItem.vue';
+
+const birthdate = new Date(1999, 9, 23);
+const age = ref(Math.floor((new Date() - birthdate) / 31536000000));
+
+let educationData = ref(cvData.education);
+let experienceData = ref(cvData.workExperience);
+</script>
+
+
 <template>
     <div class="relative right-0 h-content-window overflow-hidden">
         <div class="w-full h-full bg-white overflow-auto p-2">
@@ -15,17 +30,3 @@
         </div>
     </div>  
 </template>
-
-<script setup>
-import { ref } from 'vue';
-import ProfileHeader from './ProfileHeader.vue';
-import EducationItem from './EducationItem.vue';
-import cvData from '@/data/cv-data.json';
-import WorkExperienceItem from './WorkExperienceItem.vue';
-
-const birthdate = new Date(1999, 9, 23);
-const age = ref(Math.floor((new Date() - birthdate) / 31536000000));
-
-let educationData = ref(cvData.education);
-let experienceData = ref(cvData.workExperience);
-</script>
