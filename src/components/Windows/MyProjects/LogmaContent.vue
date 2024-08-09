015b2d7f3f56f954531020aff92f68c8df9f8286
UnMugViolet <jaguinpaul@gmail.com>
2024-08-09 12:24:41 +0200

    Fix style on content

--- src/components/Windows/MyProjects/LogmaContent.vue
+++ src/components/Windows/MyProjects/LogmaContent.vue
@@ -28,7 +28,7 @@
     <p>Logma production est une société de production audiovisuelle basée à Rennes. Fondée par Baptiste Fablet. Au début de ce projet, il existait déjà un site logma production fait par le gérant avec un site sous Wordpress.</p> <br>
     <p>La décision a été prise de refaire ce site de 0, avec une stack technique simple et éprouvée, tenter de faire un site web sans Framework et sans assistance quelquonce afin de le rendre rapide et efficace.</p> <br>
     <p>Dans le but de rendre le gérant autonome sur la gestion de ce site il a fallu développer un backoffice depuis lequel il pourrait publier de nouvelles photos, ajouter des projets et les liens vers ses vidéos</p> <br>
-    <a href="https://logma-production.com" target="_blank" class="absolute bottom-2 right-1 md:right-6 h-6 text-xxs border border-twilight-blue bg-button-submit rounded-sm leading-loose px-3 hover:shadow-button-submit-hover cursor-pointer active:bg-button-clicked">Lien vers le site </a>
+    <a href="https://logma-production.com/" target="_blank" class="absolute bottom-2 right-1 md:right-6 h-6 text-xxs border border-twilight-blue bg-button-submit rounded-sm leading-loose px-3 hover:shadow-button-submit-hover cursor-pointer active:bg-button-clicked">Lien vers le site </a>
     <h3 class="mt-5 mb-2">Objectifs</h3>
     <div>
       <li>Refonte du site web avec l'identité graphique déjà en place</li>
@@ -37,6 +37,12 @@
       <li>Optimiser le SEO</li>
       <li>Améliorer la vitesse de chargement et les performances au global</li>
       <li>Le mettre en production</li>
+    </div>
+    <div class="w-full justify-center mt-5 mb-2 flex flex-row items-center gap-0.5 cursor-pointer transition-all hover:underline hover:text-heroic-blue hover:fill-heroic-blue">
+      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" class="fill-current">
+        <path d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/>
+      </svg>
+      <a href="https://github.com/UnMugViolet/Logma" target="_blank" class="cursor-pointer">Voir le projet</a>
     </div>
   </div>
 </template>
