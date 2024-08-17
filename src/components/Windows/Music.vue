af9316beb04b2311ca4f6be801a597681ba1bdb6
UnMugViolet <jaguinpaul@gmail.com>
2024-08-17 10:03:38 +0200

    ✅ Layout of the track display done

--- src/components/Windows/Music.vue
+++ src/components/Windows/Music.vue
@@ -6,64 +6,70 @@
           <div class="flex items-center gap-5 p-1.5">
             <img v-if="playlist.images && playlist.images.length > 0" :src="playlist.images[0].url" alt="Playlist Image" class="w-24">
             <div>
-              <h2 class="text-xl font-bold"> {{ playlist.name }}</h2>
+              <h2 class="text-xl font-bold">{{ playlist.name }}</h2>
               <p class="text-xs">{{ playlist.description }}</p>
             </div>
           </div>
           <div>
-            <div class="flex items-center pl-1.5">
-              <div class="pr-5 pl-2">
-                <p class="text-xs">#</p>
-              </div>
-              <div class="w-full flex items-center">
-                <div class="w-1/3 flex flex-col max-w-48">
+            <div class="grid grid-cols-12 items-center pl-1.5">
+              <div class="col-span-5 flex items-center gap-1">
+                <div class="pl-2 w-8">
+                  <p class="text-xs">#</p>
+                </div>
+                <div class="flex flex-col max-w-48">
                   <p class="text-xs font-trebuchet-pixel">Titre</p>
                 </div>
-                <div class="w-1/3 overflow-hidden px-1">
-                  <p class="text-xs font-trebuchet-pixel pl-10">Album</p>
-                </div>
-                <div class="w-2/12 px-1">
-                  <p class="text-xs font-trebuchet-pixel text-center">Ajouté le</p>
-                </div>
-                <div class="w-2/12 px-1">
-                  <p class="text-xs font-trebuchet-pixel">Durée</p>
-                </div>
+              </div>
+              <div class="col-span-4 overflow-hidden px-1">
+                <p class="text-xs font-trebuchet-pixel">Album</p>
+              </div>
+              <div class="col-span-2 px-1">
+                <p class="text-xs font-trebuchet-pixel">Ajouté le</p>
+              </div>
+              <div class="col-span-1 px-1">
+                <p class="text-xs font-trebuchet-pixel">Durée</p>
               </div>
             </div>
-            <div class="w-full h-px bg-gray-300 mb-2 mt-1"/>
+            <div class="w-full h-px bg-gray-300 mb-2 mt-1"></div>
           </div>
           <div class="pb-16">
-            <div v-for="(track, index) in playlist.tracks.items" :key="track.track.id" class="flex flex-col">
-              <div class="flex items-center pl-1.5">
-                <div class="pr-5 pl-2">
-                  <p class="text-xs font-trebuchet-pixel">{{ index + 1 }}</p>
+            <div v-for="(track, index) in playlist.tracks.items" :key="track.track.id">
+              <div class="grid grid-cols-12 items-center pl-1.5">
+                <div class="col-span-5 flex items-center gap-1">
+                  <div class="pl-2 w-8">
+                    <p class="text-xs font-trebuchet-pixel">{{ index + 1 }}</p>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <img v-if="track.track.album.images && track.track.album.images.length > 0" :src="track.track.album.images[2].url" alt="Track Image" class="w-12">
+                    <div class="flex flex-col max-w-48">
+                      <p class="text-sm font-trebuchet-pixel">{{ track.track.name }}</p>
+                      <p class="text-xs font-trebuchet-pixel">{{ track.track.artists[0].name }}</p>
+                    </div>
+                  </div>
                 </div>
-                <img v-if="track.track.album.images && track.track.album.images.length > 0" :src="track.track.album.images[2].url" alt="Track Image" class="w-12">
-                <div class="pl-1.5 w-full flex items-center">
-                  <div class="w-1/3 flex flex-col max-w-48">
-                    <p class="text-sm font-trebuchet-pixel">{{ track.track.name }}</p>
-                    <p class="text-xs font-trebuchet-pixel">{{ track.track.artists[0].name }}</p>
-                  </div>
-                  <div class="w-1/3 overflow-hidden px-1">
-                    <p class="text-xs text-left font-trebuchet-pixel truncate">{{ track.track.album.name }}</p>
-                  </div>
-                  <div class="w-2/12 px-1">
-                    <p class="text-xs font-trebuchet-pixel truncate">{{ formatDistanceToNow(new Date(track.added_at), { locale: fr }) }}</p>
-                  </div>
-                  <div class="w-2/12 px-1">
-                    <p class="text-xs font-trebuchet-pixel">{{ (track.track.duration_ms / 60000).toFixed(2) }} min</p>
-                  </div>
+                <div class="col-span-4 overflow-hidden px-1">
+                  <p class="text-xs text-left font-trebuchet-pixel truncate">{{ track.track.album.name }}</p>
+                </div>
+                <div class="col-span-2 px-1">
+                  <p class="text-xs font-trebuchet-pixel truncate">{{ formatDistanceToNow(new Date(track.added_at), { locale: fr }) }}</p>
+                </div>
+                <div class="col-span-1 px-1">
+                  <p class="text-xs font-trebuchet-pixel">{{ (track.track.duration_ms / 60000).toFixed(2) }} min</p>
                 </div>
               </div>
-              <div v-if="index < playlist.tracks.items.length - 1" class="w-11/12 h-px bg-gradient-to-r from-blue-300 to-white my-2"/>
+              <div v-if="index < playlist.tracks.items.length - 1" class="w-11/12 h-px bg-gradient-to-r from-blue-300 to-white my-2"></div>
             </div>
           </div>
         </div>
-        <div class="w-full h-full font-trebuchet-pixel" v-else>
+        <div v-else class="w-full h-full font-trebuchet-pixel">
           <div class="w-full h-full flex items-center justify-center flex-col">
-            <p class="text-xs text-center mb-2">Pour accéder à cette fonctionnalité, <br> il est nécessaire de se connecter à Spotify..</p>
+            <p class="text-xs text-center mb-2">Pour accéder à cette fonctionnalité, <br> il est nécessaire de se
+              connecter à Spotify..</p>
             <SubmitButton @click="redirectToSpotify" class="flex items-center gap-1">
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" class="mb-px" viewBox="0 0 24 24"><path fill="#000000" d="M17.9 10.9C14.7 9 9.35 8.8 6.3 9.75c-.5.15-1-.15-1.15-.6c-.15-.5.15-1 .6-1.15c3.55-1.05 9.4-.85 13.1 1.35c.45.25.6.85.35 1.3c-.25.35-.85.5-1.3.25m-.1 2.8c-.25.35-.7.5-1.05.25c-2.7-1.65-6.8-2.15-9.95-1.15c-.4.1-.85-.1-.95-.5s.1-.85.5-.95c3.65-1.1 8.15-.55 11.25 1.35c.3.15.45.65.2 1m-1.2 2.75c-.2.3-.55.4-.85.2c-2.35-1.45-5.3-1.75-8.8-.95c-.35.1-.65-.15-.75-.45c-.1-.35.15-.65.45-.75c3.8-.85 7.1-.5 9.7 1.1c.35.15.4.55.25.85M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" class="mb-px" viewBox="0 0 24 24">
+                <path fill="#000000"
+                  d="M17.9 10.9C14.7 9 9.35 8.8 6.3 9.75c-.5.15-1-.15-1.15-.6c-.15-.5.15-1 .6-1.15c3.55-1.05 9.4-.85 13.1 1.35c.45.25.6.85.35 1.3c-.25.35-.85.5-1.3.25m-.1 2.8c-.25.35-.7.5-1.05.25c-2.7-1.65-6.8-2.15-9.95-1.15c-.4.1-.85-.1-.95-.5s.1-.85.5-.95c3.65-1.1 8.15-.55 11.25 1.35c.3.15.45.65.2 1m-1.2 2.75c-.2.3-.55.4-.85.2c-2.35-1.45-5.3-1.75-8.8-.95c-.35.1-.65-.15-.75-.45c-.1-.35.15-.65.45-.75c3.8-.85 7.1-.5 9.7 1.1c.35.15.4.55.25.85M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2" />
+              </svg>
               Se connecter à Spotify
             </SubmitButton>
             <p class="text-red-500">{{ errorMessage }}</p>
@@ -192,7 +198,7 @@ onMounted(() => {
     fetchInitialToken(authorizationCode);
   } else if (token.value) {
     fetchPlaylist();
-    console.log('Playlist:', playlist); 
+    console.log('Playlist:', playlist);
   } else {
     return;
   }
