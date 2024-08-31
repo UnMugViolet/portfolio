403cedc4b0c2dba7075d0cfcc8eb095e45d76817
UnMugViolet <jaguinpaul@gmail.com>
2024-08-31 13:26:14 +0200

    ⚡️ Refactor the SubmitButton behavior, now accepting the a as well

---
+++ src/components/Buttons/Button.vue
@@ -0,0 +1,30 @@
+<script setup>
+const emit = defineEmits(['submit']);
+
+const props = defineProps({
+  message: String,
+  isLoading: Boolean,
+  href: String,
+});
+
+const handleClick = (event) => {
+  if (!props.href) {
+    emit('submit');
+    console.log('Button clicked');
+  }
+};
+</script>
+
+<template>
+  <component 
+    :is="href ? 'a' : 'button'"
+    :href="href"
+    :target="href ? '_blank' : null"
+    @click="handleClick" 
+    :class="{ 'cursor-wait': isLoading }"
+    class="inline-flex items-center text-xs border border-twilight-blue bg-button-submit rounded-sm leading-none px-5 py-0.5 min-h-6 hover:shadow-button-submit-hover cursor-pointer active:bg-button-clicked font-trebuchet-pixel text-black"
+    :type="href ? null : 'button'"
+  >
+    <slot />
+  </component>
+</template>
