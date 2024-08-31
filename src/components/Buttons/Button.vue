5a8eb1f23449e669223fe8258929fc10a937d638
UnMugViolet <jaguinpaul@gmail.com>
2024-08-31 13:31:42 +0200

    Improve button behavior with blank

--- src/components/Buttons/Button.vue
+++ src/components/Buttons/Button.vue
@@ -5,12 +5,12 @@ const props = defineProps({
   message: String,
   isLoading: Boolean,
   href: String,
+  blank: Boolean,
 });
 
 const handleClick = (event) => {
   if (!props.href) {
     emit('submit');
-    console.log('Button clicked');
   }
 };
 </script>
@@ -19,7 +19,7 @@ const handleClick = (event) => {
   <component 
     :is="href ? 'a' : 'button'"
     :href="href"
-    :target="href ? '_blank' : null"
+    :target="blank ? '_blank' : null"
     @click="handleClick" 
     :class="{ 'cursor-wait': isLoading }"
     class="inline-flex items-center text-xs border border-twilight-blue bg-button-submit rounded-sm leading-none px-5 py-0.5 min-h-6 hover:shadow-button-submit-hover cursor-pointer active:bg-button-clicked font-trebuchet-pixel text-black"
