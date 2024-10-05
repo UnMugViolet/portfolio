05d360f510c81a247a3e9160e0fd32dfc1e37e52
UnMugViolet <jaguinpaul@gmail.com>
2024-10-05 19:18:45 +0200

    Making CV downloadable

--- src/components/Windows/MyCV/MyCV.vue
+++ src/components/Windows/MyCV/MyCV.vue
@@ -5,28 +5,41 @@ import EducationItem from './EducationItem.vue';
 import cvData from '@/data/cv-data.json';
 import WorkExperienceItem from './WorkExperienceItem.vue';
 
-const birthdate = new Date(1999, 9, 23);
-const age = ref(Math.floor((new Date() - birthdate) / 31536000000));
+const birthdate = new Date(1999, 8, 23);
+const today = new Date();
+let age = ref(today.getFullYear() - birthdate.getFullYear());
+
+// Adjust age if the current date has not passed the birthdate in the current year
+if (today.getMonth() < birthdate.getMonth() ||
+  (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate())) {
+  age.value--;
+}
 
 let educationData = ref(cvData.education);
 let experienceData = ref(cvData.workExperience);
+
+console.log(age.value);
 </script>
 
-
 <template>
-    <div class="relative right-0 h-content-window overflow-hidden">
-        <div class="w-full h-full bg-white overflow-auto p-2">
-            <div>
-                <ProfileHeader :age="age"/>
-                <section class="mt-5">
-                    <h2 class="font-trebuchet-pixel underline">Formations</h2>
-                    <EducationItem v-for="education in educationData" :key="education.id" :education="education" />
-                </section>
-                <div class="mt-3">
-                    <h2 class="font-trebuchet-pixel mt-5 underline">Expériences Professionnelles</h2>
-                    <WorkExperienceItem v-for="(workExperience, index) in experienceData" :key="index" :workExperience="workExperience"/>                
-                </div>
-            </div>
+  <div class="relative right-0 h-content-window overflow-hidden">
+    <div class="w-full h-full bg-white overflow-auto p-2">
+      <div>
+        <ProfileHeader :age="age" />
+        <section class="mt-5">
+          <h2 class="font-trebuchet-pixel underline">Formations</h2>
+          <EducationItem v-for="education in educationData" :key="education.id" :education="education" />
+        </section>
+        <div class="mt-3">
+          <h2 class="font-trebuchet-pixel mt-5 underline">Expériences Professionnelles</h2>
+          <WorkExperienceItem v-for="(workExperience, index) in experienceData" :key="index"
+            :workExperience="workExperience" />
         </div>
-    </div>  
+      </div>
+    </div>
+  </div>
+  <a rel="noopener" href="pdf/Cv Paul Jaguin.pdf" download="Cv Paul Jaguin.pdf"
+    class="absolute bottom-2 right-1 md:right-6 h-6 text-xxs border border-twilight-blue bg-button-submit rounded-sm leading-loose px-3 hover:shadow-button-submit-hover cursor-pointer active:bg-button-clicked">
+    Télécharger le CV
+  </a>
 </template>
