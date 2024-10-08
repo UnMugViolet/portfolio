e9b87f557bed0a9c0abb3bf32467365416e842ee
UnMugViolet <jaguinpaul@gmail.com>
2024-10-08 15:47:09 +0200

    Include the matomo tracking tool

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import '../css/main.css'
+import VueMatomo from 'vue-matomo'
 import '@/index.css'
 
 import { createApp } from 'vue' 
@@ -13,4 +14,12 @@ const app = createApp(App);
 app.use(pinia);
 app.use(router);
 
+// Matomo tracking
+app.use(VueMatomo, {
+  host: 'https://matomo.rocketegg.systems',
+  siteId: 1,
+});
+
 app.mount('#app');
+
+window._paq.push(['trackPageView'])
