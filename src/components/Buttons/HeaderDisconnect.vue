9c2925f636c60090a609dfc0c3d1ad77cd9553de
UnMugViolet <jaguinpaul@gmail.com>
2024-10-09 17:13:37 +0200

    ✨ Principle of back and forth with the connexion and  loader

--- src/components/Buttons/HeaderDisconnect.vue
+++ src/components/Buttons/HeaderDisconnect.vue
@@ -1,6 +1,18 @@
+<script setup>
+import { useConnectionStore } from '@/stores/connectionStore';
+
+const connectionStore = useConnectionStore();
+
+const handleDisconnect = () => {
+  connectionStore.disconnect();
+} 
+</script>
+
 <template>
-    <button class="flex text-white text-xs h-full items-center buttons-header-bottom px-1 full-screen">
-        <img src="/img/icons/key-log-icon.png" alt="Se déconnecter" class="w-fit h-6 sm:h-4/6 mr-0.5 sm:mr-1"/>
-        <p class="small-p">Se Déconnecter</p>
-    </button>
+    <router-link to="/" @click="handleDisconnect">
+        <button class="flex text-white text-xs h-full items-center buttons-header-bottom px-1 full-screen">
+            <img src="/img/icons/key-log-icon.png" alt="Se déconnecter" class="w-fit h-6 sm:h-4/6 mr-0.5 sm:mr-1"/>
+            <p class="small-p">Se Déconnecter</p>
+        </button>
+    </router-link>
 </template>
