595219e2080034d74cba66741a0150d150434c56
UnMugViolet <jaguinpaul@gmail.com>
2024-10-10 17:26:44 +0200

    Remove all the logic from the App.vue and created a dedicated component for tags

---
+++ src/MetaUpdater.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+import { useHead } from '@vueuse/head';
+import { onMounted, watch } from 'vue';
+
+const route = useRoute();
+
+const updateMetaTags = (to) => {
+  useHead({
+    title: to.meta.title || 'Default title',
+    meta: (to.meta.metaTags || []).map(tag => ({
+      name: tag.name || tag.property,
+      content: tag.content
+    }))
+  });
+};
+
+onBeforeRouteUpdate((to, from, next) => {
+  updateMetaTags(to);
+  next();
+});
+
+onMounted(() => {
+  updateMetaTags(route);
+});
+
+watch(route, (newRoute) => {
+  updateMetaTags(newRoute);
+});
+</script>
+
+<template>
+  <div></div>
+</template>
