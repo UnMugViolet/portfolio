042b4d0ae0a8c3586a2f8590c5c809bc71806d45
UnMugViolet <jaguinpaul@gmail.com>
2024-10-23 21:11:42 +0200

    ✨ Added the side menu for pictures components

--- src/components/Windows/Pictures.vue
+++ src/components/Windows/Pictures.vue
@@ -1,8 +1,15 @@
 <template>
-  <div></div>
+  <div class="relative right-0 h-content-window flex">
+    <WindowLeftMenu :leftMenuType="props.leftMenuType" />
+    <!-- Content of project -->
+    <div class="w-full h-full bg-white overflow-auto overflow-x-hidden pb-8 md:pb-5">
+
+    </div>
+  </div>
 </template>
 
 <script setup>
+import WindowLeftMenu from '@/components/Windows/WindowLeftMenu.vue'
 
 const props = defineProps({
   leftMenuType: String,
