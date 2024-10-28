a73f135b3f0e5095d527e3d2697f2bb8aa56b0c9
UnMugViolet <jaguinpaul@gmail.com>
2024-10-28 10:53:25 +0100

    🚑 Fix responsive for Pictures

--- src/components/Windows/WindowLeftMenu.vue
+++ src/components/Windows/WindowLeftMenu.vue
@@ -22,7 +22,7 @@ const getLocalizedSubtitle = (subtask) => {
 </script>
 
 <template>
-  <div class="flex flex-col flex-shrink-0 gap-3 bg-window-side-menu w-32 md:w-12.125 h-full p-2.5 overflow-auto z-20">
+  <div class="flex flex-col flex-shrink-0 gap-3 bg-window-side-menu w-32 md:w-12.125 h-full p-2.5 overflow-hidden z-20">
     <div
       v-for="item in leftMenu"
       :key="item.id"
@@ -41,14 +41,14 @@ const getLocalizedSubtitle = (subtask) => {
       "
     >
       <div v-if="item.iconHeader">
-        <img :src="item.iconHeader" :alt="getLocalizedTitle(item)" class="absolute w-4.5 h-4.5 md:w-7 md:h-7 top-3 md:top-2.5" />
+        <img :src="item.iconHeader" :alt="getLocalizedTitle(item)" class="absolute w-5 h-5 md:w-7 md:h-7 top-3 md:top-2.5" />
         <div
           :class="[
             'flex flex-row justify-between items-center bg-window-menu-card-header rounded-t-sm px-1 py-px sm:px-2 sm:py-0.5 text-xs-mobile md:text-xs font-bold cursor-pointer hover:text-heroic-blue',
             { 'bg-window-menu-card-header-active text-white hover:text-white': item['headerActive'] }
           ]"
         >
-          <h3 class="ml-4 md:ml-6 text-xxs">{{ getLocalizedTitle(item) }}</h3>
+          <h3 class="ml-4 md:ml-6 text-xs-mobile md:text-xxs">{{ getLocalizedTitle(item) }}</h3>
           <img :src="item.iconSrc" alt="plus" class="w-3.5 h-3.5 sm:w-4 sm:h-4 drop-shadow-lg" />
         </div>
       </div>
@@ -59,10 +59,10 @@ const getLocalizedSubtitle = (subtask) => {
           { 'bg-window-menu-card-header-active text-white hover:text-white': item['headerActive'] }
         ]"
       >
-        <h3 class="text-xxs">{{ getLocalizedTitle(item) }}</h3>
+        <h3 class="text-xs-mobile md:text-xxs">{{ getLocalizedTitle(item) }}</h3>
         <img :src="item.iconSrc" alt="plus" class="w-3.5 h-3.5 sm:w-4 sm:h-4 drop-shadow-lg" />
       </div>
-      <div class="px-1 py-0.75 md:px-2" v-for="subtask in item.subtasks" :key="subtask.id">
+      <div class="px-1 py-0.5 md:py-0.75 md:px-2" v-for="subtask in item.subtasks" :key="subtask.id">
         <a v-if="subtask.url" :href="subtask.url" target="_blank" class="flex flex-row items-center gap-1 cursor-pointer hover:underline hover:text-heroic-blue">
           <img :src="subtask.iconUrl" :alt="subtask.subtitle" class="w-2.5 h-2.5 sm:w-3.5 sm:h-3.5" />
           <p class="font-light text-xxs-mobile md:text-xxs">{{ getLocalizedSubtitle(subtask) }}</p>
