2e559d8ea14c280aea323949c6ace4792ad161e2
UnMugViolet <jaguinpaul@gmail.com>
2024-10-28 15:53:28 +0100

    🎨 Format and remove the console.log

--- src/components/Windows/WindowHeaderTools.vue
+++ src/components/Windows/WindowHeaderTools.vue
@@ -6,7 +6,7 @@ const emit = defineEmits()
 
 const props = defineProps({
   isGoBackAvailable: Boolean,
-  headerToolsId: String,
+  headerToolsId: String
 })
 
 const goBack = () => {
@@ -17,12 +17,6 @@ const headerTools = computed(() => {
   return headerToolsData.headerToolsItems[props.headerToolsId]
 })
 
-headerTools.value.forEach((tool, index) => {
-  console.log(`Tool ${index}:`, tool)
-  if (tool.hasBackAndForward) {
-    console.log(`Tool ${index} hasBackAndForward:`, tool.hasBackAndForward)
-  }
-})
 </script>
 
 <template>
@@ -32,18 +26,20 @@ headerTools.value.forEach((tool, index) => {
       <div v-if="tools.type === 'group'" class="flex justify-center items-center">
         <!-- Conditional rendering for back and forward icons if available -->
         <div v-if="tools.hasBackAndForward" class="flex items-center pl-1">
-          <div @click="goBack" class="flex items-center border rounded-sm pr-1.5 h-8" :class="{
-            'bg-transparent cursor-pointer hover:border-gray-300 hover:shadow-header-tools': isGoBackAvailable,
-            'filter grayscale': !isGoBackAvailable
-          }">
-            <img src="/img/icons/windows-header-tools/right-arrow-green-icon.webp"
-              :alt="$t('common.icon') + ' ' + $t('common.back')" class="w-7 h-7 flex-shrink-0" />
+          <div
+            @click="goBack"
+            class="flex items-center border rounded-sm pr-1.5 h-8"
+            :class="{
+              'bg-transparent cursor-pointer hover:border-gray-300 hover:shadow-header-tools': isGoBackAvailable,
+              'filter grayscale': !isGoBackAvailable
+            }"
+          >
+            <img src="/img/icons/windows-header-tools/right-arrow-green-icon.webp" :alt="$t('common.icon') + ' ' + $t('common.back')" class="w-7 h-7 flex-shrink-0" />
             <p class="small-p mr-2">{{ $t('common.back') }}</p>
             <div class="block border-solid down-arrow"></div>
           </div>
           <div class="flex items-center mr-1 filter grayscale">
-            <img src="/img/icons/windows-header-tools/left-arrow-green-icon.webp"
-              :alt="$t('common.icon') + ' ' + $t('common.next')" class="w-7 h-7 flex-shrink-0" />
+            <img src="/img/icons/windows-header-tools/left-arrow-green-icon.webp" :alt="$t('common.icon') + ' ' + $t('common.next')" class="w-7 h-7 flex-shrink-0" />
             <div class="block border-solid down-arrow ml-px"></div>
           </div>
         </div>
