e36dc6f98d6916198851192a31a77126106d0a4e
UnMugViolet <jaguinpaul@gmail.com>
2024-11-17 13:53:43 +0100

    rm console log

--- src/components/Windows/WindowHeaderSearch.vue
+++ src/components/Windows/WindowHeaderSearch.vue
@@ -36,7 +36,6 @@ const props = defineProps({
   isSearchVisible: Boolean
 })
 
-console.log('isSearchVisible: ', props.isSearchVisible, 'id', props.id)
 
 const formatName = (nameKey) => {
   if (!nameKey) return ''
